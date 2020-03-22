857e5ea1a503d9dfb4676bc635a9cf65cc8d54a5
nocontent <1243160+nocontent@users.noreply.github.com>
2020-03-21 18:18:41 -0700

    Add DataType attributes to Speaker

--- ConferenceTracker/Entities/Speaker.cs
+++ ConferenceTracker/Entities/Speaker.cs
@@ -9,15 +9,20 @@ namespace ConferenceTracker.Entities
         [Required]
         public int Id { get; set; }
         [Required]
+        [DataType(DataType.Text)]
         [Display(Name = "First name")]
         public string FirstName { get; set; }
         [Required]
+        [DataType(DataType.Text)]
         [Display(Name = "Last name")]
         public string LastName { get; set; }
         [Required]
+        [DataType(DataType.MultilineText)]
         public string Description { get; set; }
+        [DataType(DataType.EmailAddress)]
         [Display(Name = "Email Address")]
         public string EmailAddress { get; set; }
+        [DataType(DataType.PhoneNumber)]
         [Display(Name = "Phone Number")]
         public string PhoneNumber { get; set; }
         public bool IsStaff { get; set; }
