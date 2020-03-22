8749d11d05779e60c9c292f7802d2592d5c5c012
nocontent <1243160+nocontent@users.noreply.github.com>
2020-03-21 18:09:33 -0700

    Add required attributes to speaker

--- ConferenceTracker/Entities/Speaker.cs
+++ ConferenceTracker/Entities/Speaker.cs
@@ -6,11 +6,15 @@ namespace ConferenceTracker.Entities
 {
     public class Speaker
     {
+        [Required]
         public int Id { get; set; }
+        [Required]
         [Display(Name = "First name")]
         public string FirstName { get; set; }
+        [Required]
         [Display(Name = "Last name")]
         public string LastName { get; set; }
+        [Required]
         public string Description { get; set; }
         [Display(Name = "Email Address")]
         public string EmailAddress { get; set; }
