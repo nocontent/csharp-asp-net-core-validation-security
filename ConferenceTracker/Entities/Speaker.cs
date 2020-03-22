3160357729ab34e8ddcb22ec47de1ae206a49bb2
nocontent <1243160+nocontent@users.noreply.github.com>
2020-03-21 18:34:01 -0700

    Add email address validation

--- ConferenceTracker/Entities/Speaker.cs
+++ ConferenceTracker/Entities/Speaker.cs
@@ -4,27 +4,48 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ConferenceTracker.Entities
 {
-    public class Speaker
+    public class Speaker : IValidatableObject
     {
         [Required]
         public int Id { get; set; }
+        
         [Required]
         [DataType(DataType.Text)]
+        [StringLength(100, MinimumLength=2)]
         [Display(Name = "First name")]
         public string FirstName { get; set; }
+        
         [Required]
         [DataType(DataType.Text)]
+        [StringLength(100, MinimumLength=2)]
         [Display(Name = "Last name")]
         public string LastName { get; set; }
+        
         [Required]
+        [StringLength(500, MinimumLength=10)]
         [DataType(DataType.MultilineText)]
         public string Description { get; set; }
+        
         [DataType(DataType.EmailAddress)]
         [Display(Name = "Email Address")]
         public string EmailAddress { get; set; }
+        
         [DataType(DataType.PhoneNumber)]
         [Display(Name = "Phone Number")]
         public string PhoneNumber { get; set; }
         public bool IsStaff { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            var validationResults = new List<ValidationResult>();
+
+            if (EmailAddress != null && EmailAddress.EndsWith("TechnologyLiveConference.com", StringComparison.InvariantCultureIgnoreCase))
+            {
+                var rejectedEmailAddress = new ValidationResult("Technology Live Conference staff should not use their conference email addresses.");
+                validationResults.Add(rejectedEmailAddress);
+            }
+
+            return validationResults;
+        }
     }
 }
