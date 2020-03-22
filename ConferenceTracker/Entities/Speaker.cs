aeb66dff4a389b2b214fe3c6ef01b9fb9005d4f5
nocontent <1243160+nocontent@users.noreply.github.com>
2020-03-22 12:50:52 -0700

    Add field validation with DataAnnotations

--- ConferenceTracker/Entities/Speaker.cs
+++ ConferenceTracker/Entities/Speaker.cs
@@ -6,14 +6,31 @@ namespace ConferenceTracker.Entities
 {
     public class Speaker
     {
+        [Required]
         public int Id { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [StringLength(100, MinimumLength = 2)]
         [Display(Name = "First name")]
         public string FirstName { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [StringLength(100, MinimumLength = 2)]
         [Display(Name = "Last name")]
         public string LastName { get; set; }
+
+        [Required]
+        [DataType(DataType.MultilineText)]
+        [StringLength(500, MinimumLength = 10)]
         public string Description { get; set; }
+
+        [DataType(DataType.EmailAddress)]
         [Display(Name = "Email Address")]
         public string EmailAddress { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
         [Display(Name = "Phone Number")]
         public string PhoneNumber { get; set; }
         public bool IsStaff { get; set; }
